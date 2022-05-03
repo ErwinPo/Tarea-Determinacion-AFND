ea227506fe9f0150c140b36a21e4d9db51f782b6
unknown <A01734881@tec.mx>
2022-05-03 10:29:59 -0500

    first commit

---
+++ lib/automatatarea.ex
@@ -0,0 +1,18 @@
+defmodule Automatatarea do
+  @moduledoc """
+  Documentation for `Automatatarea`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Automatatarea.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/automatatarea_test.exs
@@ -0,0 +1,8 @@
+defmodule AutomatatareaTest do
+  use ExUnit.Case
+  doctest Automatatarea
+
+  test "greets the world" do
+    assert Automatatarea.hello() == :world
+  end
+end
