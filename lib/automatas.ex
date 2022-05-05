35ee1552033e29a0652942ef1f65b7d395fa9280
unknown <A01734881@tec.mx>
2022-05-04 22:11:57 -0500

    parte 1

--- lib/automatas.ex
+++ lib/automatas.ex
@@ -3,18 +3,14 @@ defmodule Automatas do
   def nfa1 do
     %{
       states: [P0,P1,P2,P3],
+      sigma: [?a, ?b],
       q0: P0,
       final: [P3],
-      sigma: [?a, ?b],
       delta: %{
-        {P0, "a"} => [P0, P1],
-        {P0, "b"} => [P0],
-        {P1, "a"} => [],
-        {P1, "b"} => [P2],
-        {P2, "a"} => [],
-        {P2, "b"} => [P3],
-        {P3, "a"} => [],
-        {P3, "b"} => [],
+        {P0, ?a} => [P0, P1],
+        {P0, ?b} => [P0],
+        {P1, ?b} => [P2],
+        {P2, ?b} => [P3],
       }
     }
   end
